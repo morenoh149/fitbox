5602290ac571a7bfdb54f701273029bac6b37b41
Harry Moreno <morenoh149@gmail.com>
2014-09-06 16:47:22 -0700

    style changes

--- public/script.js
+++ public/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
   $('button#test').click(sendBeep);
+  $('button#boxing').click(startBoxing);
+  $('button#stop').click(cancelRoutine);
+  initClock();
 });
+
+// initializes the clock's view
+var initClock = function() {
+  var currentTime = Date.now();
+  $('div#clock').append('00:00');
+};
 
 var sendBeep = function() {
   $.get( "https://agent.electricimp.com/A1Rf-bQt0ws9?speaker=1", function( data ) {
     $( ".result" ).html( data );
     alert( "Load was performed." );
   });
+};
+
+var startBoxing = function() {
+  //
+};
+
+var cancelRoutine = function() {
+  //
 };
