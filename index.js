e5797494e3ff866ae7d3a92ec1389cf76592a298
Harry Moreno <morenoh149@gmail.com>
2014-09-06 14:58:14 -0700

    added pg silver

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -10,4 +11,16 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
 })
